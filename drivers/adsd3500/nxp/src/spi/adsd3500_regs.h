758a29d50b5346ad684139bae92003d8c478ad9d
Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>
2025-06-26 15:38:47 +0530

    drivers: adsd3500: nxp: src: spi: adsd3500-spi.c: updated source codes for driver version 6.1.0
    Signed-off-by: Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>

--- drivers/adsd3500/nxp/src/spi/adsd3500_regs.h
+++ drivers/adsd3500/nxp/src/spi/adsd3500_regs.h
@@ -11,7 +11,7 @@
 
 #include <linux/bitfield.h>
 
-#define DRIVER_VERSION				"6.1.0"
+#define DRIVER_VERSION                          "6.1.0"
 
 #define REVERSE_SHORT(x) ((0x0000FF00 & x << 8) | (0x000000FF & x >> 8))
 #define REVERSE_LONG(x) ( ((x&0xff000000)>>24) | (((x&0x00ff0000)<<8)>>16) | (((x&0x0000ff00)>>8)<<16) | ((x&0x000000ff) << 24) )
@@ -51,6 +51,11 @@
 
 #define SET_FRAMERATE_CMD                   0x0022
 #define GET_FRAMERATE_CMD                   0x00230000
+#define ENABLE_FSYNC_TRIGGER                            0x0025
+#define FSYNC_STOP                                      0x0000
+#define FSYNC_START                                     0x0001
+#define FSYNC_HIZ_STATE                                 0x0002
+
 
 #define READ_REGISTER_CMD					0xFFFF
 #define WRITE_REGISTER_CMD					0xFFFF
@@ -58,6 +63,7 @@
 #define SWITCH_TO_BURST_CMD         	    0x00190000
 
 #define ADSD3500_CHIP_ID					0x5931
+#define ADSD3500_DEFAULT_FPS                    10
 #define USER_TASK 				_IOW('A',1,int32_t*)
 #define SIGETX 					44
 #define ADSD3500_FIRMWARE			"adi/adsd3500-fw.bin"
