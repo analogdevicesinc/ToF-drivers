64750732b56edb4ad9a3080dee3b2034211c47ea
Sivasubramanaiyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>
2026-08-14 14:29:10 +0530

    drivers: adsd3500: rpi: src: adsd3500.c: enable streaming before starting FSYNC PWM
    - Send the STREAM-ON command before enabling the FSYNC PWM output.
    - Replace the frame-rate dependent delay with a fixed 5 ms delay.
    - Ensure PWM-triggered synchronization starts only after streaming is active.
    
    Signed-off-by: Sivasubramanaiyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>

--- drivers/adsd3500/rpi/src/adsd3500.c
+++ drivers/adsd3500/rpi/src/adsd3500.c
@@ -1556,18 +1556,19 @@ static int adsd3500_start_streaming(struct adsd3500 *adsd3500)
 
 	dev_info(adsd3500->dev, "%s\n", __func__);
 
+	ret = regmap_write(adsd3500->regmap, STREAM_ON_CMD, STREAM_ON_VAL);
+	if (ret < 0) {
+		dev_err(adsd3500->dev, "Write of STREAM-ON command failed.\n");
+		return ret;
+	}
+
 	if(adsd3500->pwm_fsync != NULL && adsd3500->curr_sync_mode == PWM_TRIGGER ){
+		msleep(5);
 		ret = pwm_enable(adsd3500->pwm_fsync);
 		if (ret) {
 			dev_err(adsd3500->dev, "Could not enable FSYNC PWM\n");
 			return ret;
 		}
-	}
-
-	ret = regmap_write(adsd3500->regmap, STREAM_ON_CMD, STREAM_ON_VAL);
-	if (ret < 0) {
-		dev_err(adsd3500->dev, "Write of STREAM-ON command failed.\n");
-		return ret;
 	}
 
 	return ret;
